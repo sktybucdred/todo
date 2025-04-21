67778e1b568f6cd11bf130342d511291725128a6
Kacper Przemielewski <86023@student.pb.edu.pl>
2025-04-21 09:53:22 +0200

    Initiate project

---
+++ src/main/java/org/example/dziennikmonolith/DziennikMonolithApplication.java
@@ -0,0 +1,13 @@
+package org.example.dziennikmonolith;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DziennikMonolithApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DziennikMonolithApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/example/dziennikmonolith/DziennikMonolithApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.dziennikmonolith;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DziennikMonolithApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
